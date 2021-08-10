fe3ba489e23bff87e4cccae181437136c331d936
Gerard Hubert <mikado842@gmail.com>
2021-08-10 18:16:31 +0200

    twig et AbstractCOntroller

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,33 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Taxes\Detector;
-use App\Taxes\Calculator;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
-
-    private $calculator;
-    private $detector;
-
-    public function __construct(Calculator $calculator, Detector $detector)
-    {
-        $this->calculator = $calculator;
-        $this->detector = $detector;
-    }
 
     /**
      * @Route("/hello/{prenom<\w+>?World}", name="hello", methods={"GET", "POST"})
      */
     public function hello($prenom)
     {
-        $taxe = $this->calculator->calcul(200);
-        dump($this->detector->detect(10));
-        dump($this->detector->detect(526));
-        dump($taxe);
-        return new Response("Hello $prenom");
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom,
+        ]);
     }
 }
