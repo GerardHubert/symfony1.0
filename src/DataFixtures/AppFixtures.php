16c9027366a306dbe99aaac4350e6feb324ab310
Gerard Hubert <mikado842@gmail.com>
2021-08-14 14:49:52 +0200

    jointure avec doctrine

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Faker\Factory;
 use App\Entity\Product;
 use Liior\Faker\Prices;
@@ -29,13 +30,28 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
-        for ($i = 0; $i < 100; $i++) {
-            $product = new Product();
-            $product->setName($faker->productName())
-                ->setPrice($faker->price(200, 45000))
-                ->setSlug(strtolower($this->slugger->slug($product->getName())));
-            $manager->persist($product);
-        };
+        // On crée un boucle qui va créer 3 catégories.
+        // On fait persister les catégories
+        // Pour chaque catégorie on crée une autre boucle afin de créer 25 objet par catégorie.
+        // Comme il y a une liaison entre Product et Category avec une clé étrangere sur product, on passe l'objet category nouvellement créé dans setCategory
+        // On fait persister les produits.
+        // On flush le tout
+
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
+            $category->setName($faker->department)
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $manager->persist($category);
+
+            for ($i = 0; $i < 25; $i++) {
+                $product = new Product();
+                $product->setName($faker->productName())
+                    ->setPrice($faker->price(200, 45000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category);
+                $manager->persist($product);
+            };
+        }
 
         $manager->flush();
     }
