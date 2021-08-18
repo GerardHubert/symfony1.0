6ffffe6fc043641314d4d727a56b3083af51ab73
Gerard Hubert <mikado842@gmail.com>
2021-08-18 18:06:55 +0200

    mise en place formulaire avec FormFactoryInterface

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Doctrine\DBAL\Schema\View;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class ProductController extends AbstractController
 {
@@ -52,6 +56,52 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(FormFactoryInterface $factory): Response
+    {
+        $builder = $factory->createBuilder();
+        $builder->add('name', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => "Tapez le nom du nouveau produit"
+            ],
+            'label' => 'Nom du produit',
+        ])
+            ->add('shortDescription', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => "Tapez une courte, mais parlante, description du produit"
+                ],
+                'label' => "Description du produit"
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => "Tapez le prix en €"
+                ],
+                'label' => "Prix du produit"
+            ])
+            ->add('category', EntityType::class, [
+                'label' => "Catégorie",
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'placeholder' => "-- Selectionnez une catégorie --",
+                'class' => Category::class,
+                'choice_label' => "name"
+            ]);
+
+        $form = $builder->getForm();
+
+        $formView = $form->createView();
+
+        return $this->render("product/create.html.twig", [
+            'formView' => $formView
         ]);
     }
 }
